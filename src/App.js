68b831b0d44be3236788db0a3bf3597c2d983831
Daniel Limanowski <dlimanow@iastate.edu>
2018-06-05 11:18:34 -0500

    add styling with external and inline (JSX) css

--- src/App.js
+++ src/App.js
@@ -58,18 +58,20 @@ class App extends Component {
     render() {
         const { searchTerm, list } = this.state; // destructuring local state object using ES6
         return (
-            <div className="App">
-                <Search
-                    value={searchTerm}
-                    onChange={this.onSearchChange}
-                >
-                    Search:
-                </Search>
-                <Table
-                    list={list}
-                    pattern={searchTerm}
-                    onDismiss={this.onDismiss}
-                />
+            <div className="page">
+                <div className="interactions">
+                    <Search
+                        value={searchTerm}
+                        onChange={this.onSearchChange}
+                    >
+                        Search:
+                    </Search>
+                    <Table
+                        list={list}
+                        pattern={searchTerm}
+                        onDismiss={this.onDismiss}
+                    />
+                </div>
             </div>
         );
     }
@@ -86,17 +88,20 @@ const Search = ({ value, onChange, children }) =>
     </form>
 
 const Table = ({ list, pattern, onDismiss }) =>
-    <div>
+    <div className="table">
         {list.filter(isSearched(pattern)).map(item => // demonstrates ES6 condensed arrow function
-            <div key={item.objectID}>
-                <span>
+            <div key={item.objectID} className="table-row">
+                <span style={{ width : '40%'}}>
                     <a href={item.url}>{item.title}</a>
                 </span>
-                <span>{item.author}</span>
-                <span>{item.num_comments}</span>
-                <span>{item.points}</span>
-                <span>
-                    <Button onClick={() => this.onDismiss(item.objectID)}>
+                <span style={{ width: '30%' }}>{item.author}</span>
+                <span style={{ width: '10%' }}>{item.num_comments}</span>
+                <span style={{ width: '10%' }}>{item.points}</span>
+                <span style={{ width: '10%' }}>
+                    <Button
+                        onClick={() => onDismiss(item.objectID)}
+                        className="button-inline"
+                    >
                         DISMISS
                     </Button>
                 </span>
