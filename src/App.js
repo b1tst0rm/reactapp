21a276cb8478513faa47c50a121d0bc8c02c1766
Daniel Limanowski <dlimanow@iastate.edu>
2018-06-06 18:27:43 -0500

    converting setState to a function to avoid bugs in state mgmt

--- src/App.js
+++ src/App.js
@@ -25,6 +25,28 @@ const SORTS = {
     POINTS: list => sortBy(list, 'points').reverse(),
 };
 
+// Higher-order function that manages state for updates with searching
+const updateSearchTopStoriesState = (hits, page) => (prevState) => {
+    const { searchKey, results } = prevState;
+
+    const oldHits = results && results[searchKey]
+        ? results[searchKey].hits
+        : [];
+
+    const updatedHits = [
+        ...oldHits,
+        ...hits
+    ];
+
+    return {
+        results: {
+            ...results,
+            [searchKey]: { hits: updatedHits, page }
+        },
+        isLoading: false
+    };
+}
+
 class App extends Component {
     _isMounted = false;
 
@@ -55,24 +77,7 @@ class App extends Component {
 
     setSearchTopStories(result) {
         const { hits, page } = result;
-        const { searchKey, results} = this.state;
-
-        const oldHits = results && results[searchKey]
-            ? results[searchKey].hits
-            : [];
-
-        const updatedHits = [
-            ...oldHits,
-            ...hits
-        ];
-
-        this.setState({
-            results: {
-                ...results,
-                [searchKey]: { hits: updatedHits, page }
-            },
-            isLoading: false
-        });
+        this.setState(updateSearchTopStoriesState(hits, page));
     }
 
     fetchSearchTopStories(searchTerm, page = 0) {
